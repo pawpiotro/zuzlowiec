1b9642b9f72b0bab842828e1093d8ed1cb7a7ad9
Pawel Piotrowski <pawpiotro22@gmail.com>
2017-01-22 13:56:56 +0100

    minor changes

--- src/Main.java
+++ src/Main.java
@@ -12,20 +12,20 @@ public class Main
     public static void main(String [] args) {
         int size = 600;
 
-        /*MultiThreadAlgoEvo multiThreadAlgoEvo = new MultiThreadAlgoEvo(); // Pytanie - jak robimy z liczba watkow
+        MultiThreadAlgoEvo multiThreadAlgoEvo = new MultiThreadAlgoEvo(); // Pytanie - jak robimy z liczba watkow
         multiThreadAlgoEvo.repeatAlgo();
         System.out.println("fxr: " + AlgoEvo.getfxr() + "\n Parametry:");
         double wynik[] = AlgoEvo.getBestParams();
         for (int i =0; i < 8; i++)
-            System.out.println(wynik[i]);*/
+            System.out.println(wynik[i]);
 
-        double[] a0 = {-0.6650, -0.221507};
-        double[][] B = {{-0.5621, 0.02734}, {1.0, 0.99120}};
-        double[] c = {0.069207, -0.4319};
+        double[] a0 = {wynik[0], wynik[1]};
+        double[][] B = {{wynik[2], wynik[3]}, {wynik[4], wynik[5]}};
+        double[] c = {wynik[6], wynik[7]};
         Individual x = new Individual(a0, B, c);
         FitnessFunction function = new FitnessFunction(x, size);
         System.out.println("Fitness function returned value: "+function.testParameters());
-        function.printarray();
+        //function.printarray();
 
         //EventQueue.invokeLater(new Runnable() {
             //@Override
