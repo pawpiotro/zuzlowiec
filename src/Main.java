6bf8e732ac3e8807d609d4a82477b23d89f01b6b
DOMINIK PISKORZ <dompoczta3@gmail.com>
2017-01-22 13:51:29 +0100

    phi > 0

--- src/Main.java
+++ src/Main.java
@@ -12,14 +12,14 @@ public class Main
     public static void main(String [] args) {
         int size = 600;
 
-        MultiThreadAlgoEvo multiThreadAlgoEvo = new MultiThreadAlgoEvo(); // Pytanie - jak robimy z liczba watkow
+        /*MultiThreadAlgoEvo multiThreadAlgoEvo = new MultiThreadAlgoEvo(); // Pytanie - jak robimy z liczba watkow
         multiThreadAlgoEvo.repeatAlgo();
         System.out.println("fxr: " + AlgoEvo.getfxr() + "\n Parametry:");
         double wynik[] = AlgoEvo.getBestParams();
         for (int i =0; i < 8; i++)
-            System.out.println(wynik[i]);
+            System.out.println(wynik[i]);*/
 
-        /*double[] a0 = {-0.6650, -0.221507};
+        double[] a0 = {-0.6650, -0.221507};
         double[][] B = {{-0.5621, 0.02734}, {1.0, 0.99120}};
         double[] c = {0.069207, -0.4319};
         Individual x = new Individual(a0, B, c);
@@ -31,7 +31,7 @@ public class Main
             //@Override
             //public void run() {
         Draw tmp = new Draw(function.getCoords(), size);
-        tmp.init();*/
+        tmp.init();
     }
 
 }
