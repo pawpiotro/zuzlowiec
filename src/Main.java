2b41797ff7c07b290686e927e85b91d4c3a23bd0
Sarnapa <piotrak.michal@gmail.com>
2017-01-22 12:40:28 +0100

    Added detect collision - 1 attempt

--- src/Main.java
+++ src/Main.java
@@ -10,22 +10,22 @@ public class Main
     public static void main(String [] args) {
         int size = 600;
 
-        MultiThreadAlgoEvo multiThreadAlgoEvo = new MultiThreadAlgoEvo(); // Pytanie - jak robimy z liczba watkow
-        multiThreadAlgoEvo.repeatAlgo();
+        //MultiThreadAlgoEvo multiThreadAlgoEvo = new MultiThreadAlgoEvo(); // Pytanie - jak robimy z liczba watkow
+        //multiThreadAlgoEvo.repeatAlgo();
 
-        /*double[] a0 = {0.5, -1.0};
-        double[][] B = {{1.2, 0.6}, {0.7, 1.1}};
-        double[] c = {0.3, 0.2};
+        double[] a0 = {-0.6650, -0.221507};
+        double[][] B = {{-0.5621, 0.02734}, {1.0, 0.99120}};
+        double[] c = {0.069207, -0.4319};
         Individual x = new Individual(a0, B, c);
         FitnessFunction function = new FitnessFunction(x, size);
         System.out.println("Fitness function returned value: "+function.testParameters());
-        function.printarray();*/
+        function.printarray();
 
-        /*EventQueue.invokeLater(new Runnable() {
+        //EventQueue.invokeLater(new Runnable() {
             //@Override
             //public void run() {
         Draw tmp = new Draw(function.getCoords(), size);
-        tmp.init();*/
+        tmp.init();
     }
 
 }
--- src/ResultService/FitnessFunction.java
+++ src/ResultService/FitnessFunction.java
@@ -15,8 +15,11 @@ public class FitnessFunction {
     private double[] v = new double[2];
     private Coordinates currentCoords;
     private ArrayList<Coordinates> coords = new ArrayList<>();
+    private static final double rMin = 30.0;
+    private static final double rMax = 40.0;
 
-    class Coordinates   {
+    class Coordinates
+    {
         private double r;
         private double phi;
 
@@ -34,11 +37,31 @@ public class FitnessFunction {
         }
     }
 
+    class CoordinatesXY
+    {
+        private double coordX;
+        private double coordY;
+
+        CoordinatesXY(double coordX, double coordY)
+        {
+            this.coordX = coordX;
+            this.coordY = coordY;
+        }
+
+        public double getCoordX() {
+            return coordX;
+        }
+
+        public double getCoordY() {
+            return coordY;
+        }
+    }
+
     public FitnessFunction(Individual x1, int size){
-        double scale = size/(2.0*40.0);
+        double scale = size/(2.0*rMax);
         double initialR = 35.0*scale;
-        upperLimit = 40.0*scale;
-        lowerLimit = 30.0*scale;
+        upperLimit = rMax*scale;
+        lowerLimit = rMin*scale;
         //System.out.println("initalR = "+initialR);
         currentCoords = new Coordinates(initialR,0);
         x = x1;
@@ -70,11 +93,111 @@ public class FitnessFunction {
         currentCoords.phi += dphi;
     }
 
+    private Coordinates getIntersectionPoint()
+    {
+        Coordinates prevCoords = coords.get(coords.size() - 1);
+        double x0 = prevCoords.r * Math.cos(prevCoords.phi);
+        double y0 = prevCoords.r * Math.sin(prevCoords.phi);
+        double x1 = currentCoords.r * Math.cos(currentCoords.phi);
+        double y1 = currentCoords.r * Math.sin(currentCoords.phi);
+
+        double dx = x1 - x0;
+        double dy = y1 - y0;
+
+        // nie uwzgledniamy tutaj srodka bo jest u nas (0,0)
+        double A = dx * dx + dy * dy;
+        double B = 2 * (dx * x0 + dy * y0);
+        double C = x0 * x0 + y0 * y0 - rMin * rMin;
+
+        double det = B * B - 4 * A * C;
+        double t;
+        if ((A <= 0.0000001) || (det < 0))
+            return null;
+        else if (det == 0)
+        {
+            // jedno rozwiazanie
+            t = -B / (2 * A);
+            double intersectionX = x0 + t * dx;
+            double intersectionY = y0 + t * dy;
+            return getPolarCoordinates(intersectionX, intersectionY);
+        }
+        else
+        {
+            // dwa rozwiazania
+            t = (-B + Math.sqrt(det)) / (2 * A);
+            CoordinatesXY intersection1 = new CoordinatesXY(x0 + t * dx, y0 + t * dy);
+            t = (-B - Math.sqrt(det)) / (2 * A);
+            CoordinatesXY intersection2 = new CoordinatesXY(x0 + t * dx, y0 + t * dy);
+            double dst1 = Distance(intersection1.coordX - x0, intersection1.coordY - y0);
+            double dst2 = Distance(intersection2.coordX - x0, intersection2.coordY - y0);
+            if(dst1 < dst2)
+                return getPolarCoordinates(intersection1.coordX, intersection1.coordY);
+            else
+                return getPolarCoordinates(intersection2.coordX, intersection2.coordY);
+        }
+    }
+
+    private static double Distance(double dx, double dy)
+    {
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    private Coordinates getPolarCoordinates(double x, double y)
+    {
+        double r = Math.sqrt(x * x + y * y);
+        double phi = calculatePhi(x, y);;
+        return new Coordinates(r, phi);
+    }
+
+    private static double calculatePhi(double x, double y)
+    {
+        double a = y/x;
+        if(x > 0 && y >= 0)
+            return Math.atan(a);
+        else if (x > 0 && y < 0)
+            return Math.atan(a) + 2 * Math.PI;
+        else if (x < 0)
+            return Math.atan(a) + Math.PI;
+        else if (x == 0 && y > 0)
+            return Math.PI / 2;
+        else if (x == 0 && y < 0)
+            return 3 * Math.PI/2;
+        else
+            return 0;
+    }
+
     public double testParameters() {
         double dt = 0.001;
         double time = 0.0;
         for (;;) {
             makeStep(dt);
+            if(currentCoords.r > upperLimit)
+            {
+                currentCoords.r = upperLimit;
+                saveCoords();
+                break;
+            }
+
+            if(currentCoords.r < lowerLimit)
+            {
+                currentCoords.r = lowerLimit;
+                saveCoords();
+                break;
+            }
+
+            /*if(coords.size() > 0)
+            {
+                Coordinates intersectionPoint = getIntersectionPoint();
+                if (intersectionPoint != null)
+                {
+                    //System.out.println(intersectionPoint.r + " " + intersectionPoint.phi);
+                    currentCoords.r = intersectionPoint.r;
+                    currentCoords.phi = intersectionPoint.phi;
+                    saveCoords();
+                    break;
+                }
+            }*/
+
             saveCoords();
             /*System.out.println( "v[0]= " + v[0] + "\n" +
                                 "v[1]= " + v[1] + "\n" +
@@ -82,7 +205,7 @@ public class FitnessFunction {
                                 "phi = " + currentCoords.phi + "\n");*/
             if (currentCoords.phi > 2*Math.PI) break;
 
-            if(currentCoords.r > upperLimit || currentCoords.r < lowerLimit) break;
+
 
             time += dt;
         }
@@ -100,7 +223,7 @@ public class FitnessFunction {
 
     public void printarray(){
         for(Coordinates c: coords){
-            //System.out.println(c.getR()+" "+c.getPhi());
+            System.out.println(c.r + " " + c.phi);
         }
     }
 }
